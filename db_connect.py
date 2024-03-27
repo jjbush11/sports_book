bb62e5be5351f202ba5571c1ca67a9078dfdf6ea
jamesbush <jjbush@uvm.edu>
2024-03-27 17:33:21 -0400

    Able to push to database, now need to edit info of existing users and clean up functions

--- db_connect.py
+++ db_connect.py
@@ -1,11 +1,76 @@
+from sqlalchemy import (create_engine, MetaData, Table,
+                        Column, String, Float, JSON, and_,
+                        insert)
+
+
 class ConnectDb:
     """
     ConnectDb - forms connection to database.
     """
 
-    def __init__(self):
-        pass
+    db_url = 'mysql+mysqlconnector://jjbush_admin:ANo9Eju{_&4-.V+8Z%OP@webdb.uvm.edu/JJBUSH_sports_book'
+    connection = 'default'
+    user_info = 'empty table'
 
-    # Instance method (operate on instance attributes, can also access class attributes)
-    def query(self, other_parameters):
-        pass
+    def __init__(self):
+        # Create engine and connect
+        db_engine = create_engine(self.db_url)
+        self.connection = db_engine.connect()
+
+        # Define metadata
+        metadata = MetaData()
+
+        # Set up table framework
+        self.user_info = Table(
+            'user_info',
+            metadata,
+            Column('username', String(30), primary_key=True),
+            Column('password', String(30)),
+            Column('balance', Float),
+            Column('active_bets', JSON),
+            Column('resolved_bets', JSON)
+        )
+
+    # Closing connection
+    def __del__(self):
+        self.connection.close()
+
+        # Instance method (operate on instance attributes, can also access class attributes)
+    def get_row_by_user(self, par_user):
+        """
+        Takes username and returns the row of the user
+        :param: par_user, str
+        :return: if user found returns user info
+        if user not found returns None
+        """
+        try:
+            query = self.user_info.select().where(self.user_info.c.username == par_user)
+            result = self.connection.execute(query)
+            rows = result.fetchall()[0]
+
+            return rows
+        except:
+            return None
+
+    def add_new_user(self, username, password, balance=20, active_bets=None, resolved_bets=None):
+
+        # Check that username does not already exist
+
+        # Create a new user object with the provided data
+        new_user = insert(self.user_info).values(
+            username= username,
+            password= password,
+            balance= balance,
+            active_bets= active_bets,
+            resolved_bets= resolved_bets
+        )
+
+        # Insert the new user into the user_info table
+        self.connection.execute(new_user)
+        self.connection.commit()
+
+        # Check if user was added
+        rows = self.get_row_by_user(username)
+        if (rows!=None):
+            return True
+        return False
--- db_verify_connect.py
+++ db_verify_connect.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, String, Float, JSON, and_
+
+
+from sqlalchemy import create_engine, MetaData, Table, Column, String, Float, JSON, and_, insert, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -34,6 +36,52 @@ try:
 
     for row in rows:
         print(row)
+
+    print("*******Add User**********\n")
+
+    # Insert data into the table
+    insert_info = {
+        'username': 'john_doe',
+        'password': 'password123',
+        'balance': 1000.0,
+        'active_bets': [{'bet_id': 1, 'amount': 50.0}, {'bet_id': 2, 'amount': 75.0}],
+        'resolved_bets': []
+    }
+
+    # insert_info2 = (
+    #     username= 'john_doe',
+    #     password='password123',
+    #     balance= 1000.0,
+    #     active_bets= [{'bet_id': 1, 'amount': 50.0}, {'bet_id': 2, 'amount': 75.0}],
+    #     resolved_bets= []
+    # )
+
+    # Insert data into the table
+    # insert_statement = str(user_info.insert().values(insert_info))
+    insert_statement = """
+    INSERT INTO user_info (username, password, balance, active_bets, resolved_bets) 
+    VALUES (:username, :password, :balance, :active_bets, :resolved_bets)
+    """
+    # insert_edit = ''
+    # for key, value in insert_info.items():
+    #     word_to_replace = ':'+key
+    #     insert_statement+= insert_statement.replace(word_to_replace, str(insert_info[key]))
+
+    # insert_statement = text(insert_statement_str).bindparams(username='john_doe', password='password123',
+    #                                                          balance=1000.0, active_bets=[{'bet_id': 1, 'amount': 50.0}, {'bet_id': 2, 'amount': 75.0}],
+    #                                                          resolved_bets=[])
+    insert2 = insert(user_info).values(
+        username='letsGooo',
+        password='password123',
+        balance=210.0,
+        active_bets=[{'bet_id': 1, 'amount': 50.0}, {'bet_id': 2, 'amount': 75.0}],
+        resolved_bets=[]
+    )
+    # print(insert2.compile())
+    # insert_statement.replace(':username', insert_info['username']))
+    # # Execute the insert statement
+    connection.execute(insert2)
+    connection.commit()
 
     # Close the connection
     connection.close()
