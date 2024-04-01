e9557b0c53542f9d411b018868dd024bf3dfeed0
mason-eschleman <meschlem@uvm.edu>
2024-03-31 21:55:24 -0400

    Fixed up UI logic and initialization code. Cleaned up to make it less messy. Works better.

--- load_home_window.py
+++ load_home_window.py
@@ -1,37 +1,49 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 import sys
 from home_window_ui import Ui_MainWindow
 from nhl_window_ui import nhl_MainWindow
 
-def nhl_clicked():
-    nhl_app = QApplication([])
-    nhl_window = QMainWindow()
-    nhl_ui = nhl_MainWindow()
-    nhl_ui.setupUi(nhl_window)
-    nhl_window.show()
-    sys.exit(nhl_app.exec())
 
-class home_UI (QWidget):
+class home_UI (QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initialize_home_window()
 
-        self.main_ui = Ui_MainWindow()
-        self.main_ui.setupUi(self)
-        self.main_ui.pushButton_6.clicked.connect(nhl_clicked())
+    def initialize_home_window(self):
+        app = QApplication([])
+        home_window = QMainWindow()
+        ui = Ui_MainWindow()
+        ui.setupUi(home_window)
+        home_window.show()
+        app.exec()
 
+
+class nhl_Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_nhl_window()
+
+    def initialize_nhl_window(self):
+        nhl_app = QApplication([])
+        nhl_window = QMainWindow()
+        nhl_ui = nhl_MainWindow()
+        nhl_ui.setupUi(nhl_window)
+        nhl_window.show()
+        sys.exit(nhl_app.exec())
+
+        # TODO: James, insert database connection stuff here. I'm not sure if this is where it will go in the final
+        #  product, but just insert it here for now and I will move it later if I need to. If you have any trouble with
+        #   anything, please lmk, I'm happy to help. Or even if you wanna just tell me what I'm supposed to do to link it
+        #   to the DB and I can do it myself too if you are having issues.
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(window)
+    app = QApplication(sys.argv)
+    window = home_UI()
     window.show()
-    app.exec()
+    sys.exit(app.exec())
 
-    # TODO: James, insert database connection stuff here. I'm not sure if this is where it will go in the final
-    #  product, but just insert it here for now and I will move it later if I need to. If you have any trouble with
-    #   anything, please lmk, I'm happy to help. Or even if you wanna just tell me what I'm supposed to do to link it
-    #   to the DB and I can do it myself too if you are having issues.
+
+
 
 
 
