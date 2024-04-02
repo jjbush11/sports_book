775f94fc020db15a6ab5e62b2e2047383eade31e
jamesbush <jjbush@uvm.edu>
2024-04-02 15:04:12 -0400

    db_connect_matches.py seems complete need to upload real data now

--- display_correct_matches.py
+++ display_correct_matches.py
@@ -3,8 +3,15 @@ from db_connect_matches import ConnectDbMatch
 db_match = ConnectDbMatch()
 print("**** Display Settled Games ****")
 
+# db_match.edit_upcoming_match_row('JacketsAvalanche', 'time', '19:30:00')
+# db_match.edit_upcoming_match_row('DucksCanucks', 'time', '20:00:00')
+
 all_matches = db_match.get_all_matches()
 for match in all_matches:
-    if match.is_settled:
-        print(match.team1 + " vs " + match.team2)
-
+    db_match.check_and_set_if_match_settled(match.id)
+    # print(match.id)
+    # if match.is_settled:
+    #     print("Settled Game: " + match.team1 + " vs " + match.team2)
+    #
+    # elif not match.is_settled:
+    #     print("Not settled Game: " + match.team1 + " vs " + match.team2)
