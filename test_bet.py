4e1a370fd4ed519128fe2c9d7259e4d0645e6fda
jamesbush <jjbush@uvm.edu>
2024-04-03 16:14:16 -0400

    Organized files

--- test_bet.py
+++
@@ -1,5 +0,0 @@
-from bet import Bet
-
-bet = Bet(win=0, odds=1000, wager=10)
-
-print(type(bet.id))
--- upcoming_matches_to_db.py
+++ upcoming_matches_to_db.py
@@ -1,8 +1,8 @@
+import web_scraper
 from db_connect_matches import ConnectDbMatch
-from web_scraper import grab_nhl_moneylines, grab_nhl_scores
+from web_scraper import grab_scores, grab_moneylines
 
 db_match = ConnectDbMatch()
-
 
 def display_upcoming_matches():
     all_matches = db_match.get_all_matches()
@@ -11,12 +11,16 @@ def display_upcoming_matches():
         print(match.id)
         if match.is_settled:
             print("Settled Game: " + match.team1 + " vs " + match.team2)
-
         elif not match.is_settled:
             print("Not settled Game: " + match.team1 + " vs " + match.team2)
 
 
 def upload_current_matches_and_moneyline_db():
+
+    # List of sport scores urls
+    scores_links = [web_scraper.NBA_SCORES_URL, web_scraper.NHL_SCORES_URL,
+                    web_scraper.MLB_SCORES_URL]
+
     # Get lists of winning and losing teams with scores
     teams_scores = grab_nhl_scores()
     print(teams_scores)
