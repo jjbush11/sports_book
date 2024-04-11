d0765cb930d8239d77a4e4f1b23a1270533d3965
jbrennan976 <jbrennan9763@gmail.com>
2024-04-11 17:33:11 -0400

    Login database interactivity completed

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 import homepage
+import db_connect_user
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit, QHBoxLayout, QGridLayout, QMessageBox
 from PyQt6.QtGui import QFont
 from PyQt6.QtCore import Qt
 
@@ -107,15 +108,20 @@ class LoginWindow(QMainWindow):
         self.start_window.show()
         self.close()
 
-    def submit_click(self):
-        print (self.username.text())
-        print (self.password.text())
-        #Logic to check user credentials against user_db
+    def submit_click(self, layout):
+        username = self.username.text()
+        password = self.password.text()
 
-        #If passed, route to main application window
-        self.home_window = homepage.StartWindow()
-        self.home_window.show()
-        self.close()
+       # Logic to check user credentials against user_db
+        if(db_connect_user.does_user_exist(username)):
+            #If passed, route to main application window
+            self.home_window = homepage.StartWindow()
+            self.home_window.show()
+            self.close()
+        else:
+            QMessageBox.warning(self, 'Error', 'Error: User credentials not found in database, please check your username and password and try again')
+
+            
 
 class SignInWindow(QMainWindow):
     def __init__(self):
@@ -179,16 +185,25 @@ class SignInWindow(QMainWindow):
         self.close()     
 
     def submit_click(self):
-        print (self.username.text())
-        print (self.password.text())   
-        print (self.password2.text())
+        username = self.username.text()
+        password = self.password.text()  
+        password_check = self.password2.text()
 
-        passed = self.check_match(self.password.text(), self.password2.text())
-        #if (passed == False):
+        passed = self.check_match(password, password_check)
+        if (passed == False):
             #display message that passwords don't match
-        #else
+            QMessageBox.warning(self, 'Error', 'Error: Passwords do not match, please try again')
+        else:  
             #logic to put created username and password into user_db
-    
+            status = db_connect_user.add_new_user(username, password)
+            match (status):
+                case 0:
+                    QMessageBox.information(self, 'Success', 'Success: User credentials successfully created, please login to access your account')
+                case 1:
+                    QMessageBox.warning(self, 'Error', 'Error: User credentials already exist, please choose a new username and password')
+                case _:
+                    QMessageBox.warning(self, 'Error', 'Error: Unknown error creating user credentials, please try again')
+
     def check_match(self, password1, password2):
         if (password1 == password2):
             return True
