6262ccb3d4bdc3185bbad05f098ae270a64e9241
mason-eschleman <meschlem@uvm.edu>
2024-04-12 16:22:58 -0400

    bugs

--- load_home_window.py
+++ load_home_window.py
@@ -1,16 +1,10 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 import sys
-from home_window_ui import Ui_MainWindow
+from home_window_UI import Ui_MainWindow
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Ui_MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
